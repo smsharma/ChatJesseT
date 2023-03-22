88f57ac4fc88265a8b9ed3cad30e1ecc85c3763a
Siddharth Mishra-Sharma <smsharma@mit.edu>
2023-03-22 00:36:18 -0400

    Aesthetic updates

--- main.py
+++ main.py
@@ -14,5 +14,10 @@ def index():
         return render_template("index.html")
 
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
